b8ecd86a1dd214e38e7b0fbd539708e89e6711d4
Arman Khoshnevis <khoshne1@msu.edu>
2024-12-02 12:34:42 -0500

    Add the path to Figures

--- StreamlitApp/app.py
+++ StreamlitApp/app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../Figures')))
 
 # Set up the main structure of the Streamlit App
 def main():
